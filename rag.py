98cc17b5dc783304c8e5f04be0303a3181839a37
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-19 14:31:48 -0300

    tentando tentando

--- rag.py
+++ rag.py
@@ -1,16 +1,58 @@
 from dotenv import load_dotenv
 import os
 import numpy as np
-from langchain.vectorstores import FAISS 
+from langchain.vectorstores import Chroma
+from langchain.retrievers import ParentDocumentRetriever
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.storage import InMemoryStore
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import PyPDFLoader 
+from langchain.document_transformers import Html2TextTransformer
+from langchain.document_loaders import AsyncHtmlLoader
+
+
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
 
-vec_db = FAISS.load_local("noic_index", embeddings, allow_dangerous_deserialization=True)
+def ainvoke(prompt):
+    urls = ["https://noic.com.br/olimpiadas/", "https://noic.com.br/olimpiadas/astronomia/", "https://noic.com.br/astronomia/guia/", "https://noic.com.br/astronomia/guia/oba/", "https://noic.com.br/astronomia/guia/vinhedo/", "https://noic.com.br/astronomia/guia/antigos-e-olaa/"]
+    loader = AsyncHtmlLoader(urls)
+    docs = loader.load()
+    html2text = Html2TextTransformer()
+    docs_transformed = html2text.transform_documents(docs)
 
-retriever = vec_db.as_retriever()
+    loaderonline = PyPDFLoader('./Guia_NOIC_Seletivas_Online.pdf')
+    loaderbarra = PyPDFLoader('./guiabarra.pdf')
+
+    online = loaderonline.load()
+    barra = loaderbarra.load()
+
+    barraonline = barra + online
+
+    todes = barraonline + docs_transformed
+    vec_db = Chroma(
+        collection_name = 'split_parents', embedding_function = embeddings
+    )
+
+    parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
+
+    child_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
+
+    store = InMemoryStore()
+
+    retriever = ParentDocumentRetriever(
+        vectorstore = vec_db,
+        docstore = store,
+        child_splitter = child_splitter,
+        parent_splitter = parent_splitter,
+    )
+    retriever.add_documents(todes)
+
+    return retriever.invoke(prompt)
+
+print(ainvoke('seletiva'))
 
 def junta_docs(docs):
     return '\n\n'.join([doc.page_content for doc in docs])
@@ -20,5 +62,5 @@ def get_prompt(query):
 
     Query: {query}
 
-    Contexto:{junta_docs(retriever.invoke(query))}"""
+    Contexto:{junta_docs(ainvoke(query))}"""
 
